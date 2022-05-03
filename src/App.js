2c82c12a78c5b09d1e798dff877af2e1a95eb8c3
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-03 14:20:00 -0700

    folders set up, imported into App.js and state set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import FoodDropdown from './FoodDropdown';
+import DrinkDropdown from './DrinkDropdown';
+import SideDropdown from './SideDropdown';
+import InstructionsList from './InstructionsList';
+import InstructionsForm from './InstructionsForm';
+import OrderNameInput from './OrderNameInput';
+import OrderName from './OrderName';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+  const [sideId, setSetId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [order, setOrder] = useState('');
+  const [instructions, setInstructions] = useState([]);
+  const [instructionsForm, setInstructionsForm] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
